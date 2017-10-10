9fa11d1cef85ed83205593cca9c54a8d79e19476
Nick Clifford <nick@nickclifford.me>
2017-10-10 09:23:16 -0500

    fix postToEndpoint request body

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,10 @@ import * as request from 'request';
 
 export function postToEndpoint(endpoint: string, data?: object): Promise<any> {
 	return new Promise((resolve, reject) => {
-		request({
+		request.post({
 			url: 'https://api.mymicds.net' + endpoint,
-			method: 'POST',
-			json: true // auto-parses JSON response
+			json: true, // auto-parses JSON response and auto-serializes JSON body
+			body: data
 		}, (err, res, body) => {
 			if (err) {
 				reject(err);
